a6e7ae28a7700e368e09c6fd6e3ae972b2eb4df8
KrishnaIndia <krishnaof1988@gmail.com>
2015-01-06 12:35:48 +0530

    Add button event registration integrated

--- src/main/java/org/jenkinsci/plugins/MaidsafeJenkins/actions/GitHubTargetParameterAction.java
+++ src/main/java/org/jenkinsci/plugins/MaidsafeJenkins/actions/GitHubTargetParameterAction.java
@@ -2,32 +2,21 @@ package org.jenkinsci.plugins.MaidsafeJenkins.actions;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
-
-import jenkins.model.Configuration;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
-
 import org.jenkinsci.plugins.MaidsafeJenkins.MaidsafeJenkinsBuilder;
-import org.jenkinsci.plugins.MaidsafeJenkins.MaidsafeJenkinsBuilder.DescriptorImpl;
 import org.jenkinsci.plugins.MaidsafeJenkins.util.TargetBuildParameterUtil;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 import org.kohsuke.stapler.export.Exported;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.mapper.Mapper;
-import com.thoughtworks.xstream.persistence.XmlMap;
-
 import hudson.Functions;
 import hudson.model.Action;
 import hudson.model.AbstractProject;
 import hudson.model.Cause;
 import hudson.model.CauseAction;
-import hudson.model.Descriptor;
 import hudson.model.Hudson;
-import hudson.util.ListBoxModel;
+
 
 public class GitHubTargetParameterAction implements Action {	
 	private AbstractProject<?, ?> project;	
@@ -36,25 +25,33 @@ public class GitHubTargetParameterAction implements Action {
 		this.project = project;			
 	}	
 	
+	private String getValueFromConfig(String fieldName) {
+		String fieldValue = "";
+		try{
+			String PACKAGE = "org.jenkinsci.plugins.MaidsafeJenkins.MaidsafeJenkinsBuilder";			 
+			String xml = project.getConfigFile().asString();
+			xml = xml.substring(xml.indexOf(PACKAGE), xml.lastIndexOf(PACKAGE));			
+			xml = xml.substring(xml.indexOf(fieldName), xml.lastIndexOf(fieldName));			
+			fieldValue = xml.substring(xml.indexOf(">")+1, xml.indexOf("<"));
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}		
+		return fieldValue;
+	}
+	
 	@Exported
 	public String getProjectName() {
 		return project.getName();
 	}	
+	
+	@Exported
+	public String getOrgName() {		
+		return getValueFromConfig("orgName");
+	}
 		
 	@Exported
 	public String getDefaultBaseBranch() {
-		String baseBranch = "";
-		try{
-			String PACKAGE = "org.jenkinsci.plugins.MaidsafeJenkins.MaidsafeJenkinsBuilder";
-			String field = "defaultBaseBranch";			
-			String xml = project.getConfigFile().asString();
-			xml = xml.substring(xml.indexOf(PACKAGE), xml.lastIndexOf(PACKAGE));			
-			xml = xml.substring(xml.indexOf(field), xml.lastIndexOf(field));			
-			baseBranch = xml.substring(xml.indexOf(">")+1, xml.indexOf("<"));
-		}catch(Exception ex) {
-			ex.printStackTrace();
-		}		
-		return baseBranch;
+		return getValueFromConfig("defaultBaseBranch");
 	}
 	
 	@Exported
--- src/main/webapp/js/buildnow.js
+++ src/main/webapp/js/buildnow.js
@@ -1,43 +1,68 @@
 var MAIDSafe = {};
-MAIDSafe.org = 'maidsafe'; // TODO set value from plugin
-MAIDSafe.loadRepoOwners = function(repoSelect) {	
-	var ownerSelect = jQuery(':input:eq(' + (jQuery(':input').index(repoSelect) + 1) + ')');
-	var setOwners = function (data) {
+MAIDSafe.loadRepoOwners = function(repoSelect) {
+	var ownerSelect = jQuery(':input:eq('
+			+ (jQuery(':input').index(repoSelect) + 1) + ')');
+	var setOwners = function(data) {
 		var options = '';
-		for(var i = 0; i < data.length; i++) {		
-			options += '<option id="' + data[i].owner.login + '">' + data[i].owner.login + '</option>';
+		options += '<option id="' + MAIDSafe.org + '">' + MAIDSafe.org
+				+ '</option>';
+		for (var i = 0; i < data.length; i++) {
+			options += '<option id="' + data[i].owner.login + '">'
+					+ data[i].owner.login + '</option>';
 		}
 		ownerSelect.append(options);
+		MAIDSafe.loadBranches(ownerSelect);
 	};
 	ownerSelect.children().remove();
 	MAIDSafe.getForks(repoSelect.val(), setOwners);
 };
-MAIDSafe.registerEvents = function(element) {
+MAIDSafe.loadBranches = function(element) {
+	var ownerSelect = jQuery(element);
+	var repoSelect = jQuery(':input:eq('
+			+ (jQuery(':input').index(ownerSelect) - 1) + ')');
+	var branchesSelect = jQuery(':input:eq('
+			+ (jQuery(':input').index(ownerSelect) + 1) + ')');
+	var setBranches = function(data) {
+		var options = '';
+		for (var i = 0; i < data.length; i++) {
+			options += '<option id="' + data[i].name + '">' + data[i].name
+					+ '</option>';
+		}
+		branchesSelect.append(options);
+	};
+	branchesSelect.children().remove();
+	MAIDSafe.getBranches(repoSelect.val(), ownerSelect.val(), setBranches);
+};
+MAIDSafe.AddClickObserver = function(repoSelectElement) {
+	this.register = function() {		
+		setTimeout(function() {
+			console.log(jQuery(':input:eq('
+					+ (jQuery(':input').index(repoSelectElement) + 5) + ')'));
+			MAIDSafe.loadRepos(jQuery(':input:eq('
+					+ (jQuery(':input').index(repoSelectElement) + 5) + ')'));
+		}, 100);
+	};
+};
+MAIDSafe.registerEvents = function(element) {	
+	var buttons;
 	jQuery(element).on('change', function(event) {
 		MAIDSafe.loadRepoOwners(jQuery(event.target));
 	});
-	jQuery(':input:eq(' + (jQuery(':input').index(element) + 1) + ')').on('change', function(event) {
-		var ownerSelect = jQuery(event.target);
-		var repoSelect = jQuery(':input:eq(' + (jQuery(':input').index(event.target) - 1) + ')');
-		var branchesSelect = jQuery(':input:eq(' + (jQuery(':input').index(event.target) + 1) + ')');
-		var setBranches = function(data) {
-			var options = '';
-			for(var i = 0; i < data.length; i++) {		
-				options += '<option id="' + data[i].owner.login + '">' + data[i].owner.login + '</option>';
-			}
-			ownerSelect.append(options);
-		};
-		branchesSelect.children().remove();
-		MAIDSafe.getBranches(repoSelect.val(), ownerSelect.val(), setBranches);
-	});
+	jQuery(':input:eq(' + (jQuery(':input').index(element) + 1) + ')').on(
+			'change', function(event) {
+				MAIDSafe.loadBranches(jQuery(event.target));
+			});
+	buttons = jQuery('button');
+	jQuery(buttons[buttons.length - 3]).on('click', new MAIDSafe.AddClickObserver(element).register);
 };
-MAIDSafe.loadRepos = function(element) {	    			    		
+MAIDSafe.loadRepos = function(element) {
 	var options = '';
 	if (!MAIDSafe.hasOwnProperty('repos')) {
 		return;
-	}		
-	for(var i = 0; i < MAIDSafe.repos.length; i++) {		
-		options += '<option id="' + MAIDSafe.repos[i].name + '">' + MAIDSafe.repos[i].name + '</option>';
+	}
+	for (var i = 0; i < MAIDSafe.repos.length; i++) {
+		options += '<option id="' + MAIDSafe.repos[i].name + '">'
+				+ MAIDSafe.repos[i].name + '</option>';
 	}
 	if (!element) {
 		element = jQuery('select[repo="true"]');
@@ -46,42 +71,46 @@ MAIDSafe.loadRepos = function(element) {
 	MAIDSafe.registerEvents(element);
 	MAIDSafe.loadRepoOwners(element);
 };
-MAIDSafe.getRepositories = function() {	    			
+MAIDSafe.getRepositories = function() {
 	jQuery.ajax({
-		url:'https://api.github.com/orgs/' + MAIDSafe.org + '/repos',	    				
-	 	crossDomain: true,
-	 	headers: {
-			'Authorization': 'token ' + MAIDSafe.token
+		url : 'https://api.github.com/orgs/' + MAIDSafe.org + '/repos',
+		crossDomain : true,
+		headers : {
+			'Authorization' : 'token ' + MAIDSafe.token
 		}
-	})
-	.success(function(data) {
+	}).success(function(data) {
 		MAIDSafe.repos = data;
-		MAIDSafe.loadRepos();			
+		MAIDSafe.loadRepos();
 	})
 };
-MAIDSafe.getForks = function(repoName, callback) {	
-	jQuery.ajax({
-		url:'https://api.github.com/repos/' + MAIDSafe.org + '/' + repoName + '/forks',	    				
-	 	crossDomain: true,
-	 	headers: {
-			'Authorization': 'token ' + MAIDSafe.token
-		}
-	})
-	.success(callback);	
+MAIDSafe.getForks = function(repoName, callback) {
+	jQuery.ajax(
+			{
+				url : 'https://api.github.com/repos/' + MAIDSafe.org + '/'
+						+ repoName + '/forks',
+				crossDomain : true,
+				headers : {
+					'Authorization' : 'token ' + MAIDSafe.token
+				}
+			}).success(callback);
 };
 MAIDSafe.getBranches = function(repo, owner, callback) {
-	jQuery.ajax({
-		url:'https://api.github.com/repos/' + owner + '/' + repo + '/forks',	    				
-	 	crossDomain: true,
-	 	headers: {
-			'Authorization': 'token ' + MAIDSafe.token
-		}
-	})
-	.success(callback);	
+	jQuery.ajax(
+			{
+				url : 'https://api.github.com/repos/' + owner + '/' + repo
+						+ '/branches',
+				crossDomain : true,
+				headers : {
+					'Authorization' : 'token ' + MAIDSafe.token
+				}
+			}).success(callback);
 };
-jQuery('document').ready(function() {	    			
-	var element = jQuery('span#tempStore');	    			
+jQuery('document').ready(function() {
+	var element = jQuery('span#tempStore');
 	MAIDSafe.token = element.text();
+	element.remove();
+	element = jQuery('span#tempOrg');
+	MAIDSafe.org = element.text();
 	element.remove();
 	MAIDSafe.getRepositories();
 });
